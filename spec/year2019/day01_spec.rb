433902b3efb03860c273acf07ed05283e97fbcbc
clemkeirua <clement@keiruaprod.fr>
2019-12-01 13:39:16 +0100

    solved part2

--- spec/year2019/day01_spec.rb
+++ spec/year2019/day01_spec.rb
@@ -19,7 +19,21 @@ RSpec.describe Year2019::Day01 do
     expect(day.part1(input)).to eq(2 + 2 + 654 + 33_583)
   end
 
+  it 'computes the adjusted fuel out of a mass' do
+    expect(day.calc_fuel_adjusted(12)).to eq(2)
+    expect(day.calc_fuel_adjusted(14)).to eq(2)
+    expect(day.calc_fuel_adjusted(1969)).to eq(654 + 216 + 70 + 21 + 5)
+    expect(day.calc_fuel_adjusted(100_756)).to eq(33_583 + 11_192 + 3_728 +
+                                              1_240 + 411 + 135 + 43 + 12 + 2)
+  end
+
   it 'solves part2' do
-    expect(day.part2('some_input')).to eq(nil)
+    input = <<~INPUT
+      14
+      12
+      1969
+      100756
+    INPUT
+    expect(day.part2(input)).to eq(2 + 2 + 966 + 50_346)
   end
 end
--- src/year2019/day01.rb
+++ src/year2019/day01.rb
@@ -1,6 +1,10 @@
 module Year2019
   # Solutions for day 01 of year 2019
   class Day01
+    def initialize
+      @fuels = {}
+    end
+
     def calc_fuel(m)
       m.to_i / 3 - 2
     end
@@ -11,8 +15,20 @@ module Year2019
       end.sum
     end
 
-    def part2(_input)
-      nil
+    def calc_fuel_adjusted(m)
+      fuel = calc_fuel(m)
+      total_fuel = 0
+      while fuel >= 0
+        total_fuel += fuel
+        fuel = calc_fuel(fuel)
+      end
+      total_fuel
+    end
+
+    def part2(input)
+      input.lines.map do |n|
+        calc_fuel_adjusted(n.to_i)
+      end.sum
     end
   end
 end

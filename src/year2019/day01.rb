59d80db96d23b89540bd56b9835078243bb7e7a0
clemkeirua <clement@keiruaprod.fr>
2019-12-01 22:17:36 +0100

    more idiomatic solution

--- src/year2019/day01.rb
+++ src/year2019/day01.rb
@@ -1,18 +1,12 @@
 module Year2019
   # Solutions for day 01 of year 2019
   class Day01
-    def initialize
-      @fuels = {}
-    end
-
     def calc_fuel(m)
-      m.to_i / 3 - 2
+      m / 3 - 2
     end
 
     def part1(input)
-      input.lines.map do |n|
-        calc_fuel(n.to_i)
-      end.sum
+      input.lines.map(&:to_i).sum { |n| calc_fuel(n) }
     end
 
     def calc_fuel_adjusted(m)
@@ -26,9 +20,7 @@ module Year2019
     end
 
     def part2(input)
-      input.lines.map do |n|
-        calc_fuel_adjusted(n.to_i)
-      end.sum
+      input.lines.map(&:to_i).sum { |n| calc_fuel_adjusted(n) }
     end
   end
 end

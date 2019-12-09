ccfeb34e5e45ca8a4d1ef946b60438efe85ce9cb
clemkeirua <clement@keiruaprod.fr>
2019-12-09 22:18:50 +0100

    code style

--- src/year2019/day08.rb
+++ src/year2019/day08.rb
@@ -6,7 +6,7 @@ module Year2019
     end
 
     def find_layers(input, w, h)
-      split_by(input, w*h)
+      split_by(input, w * h)
     end
 
     def split_layer(layer, w)
@@ -14,31 +14,34 @@ module Year2019
     end
 
     def part1(input)
-      w,h = [25, 6]
+      w = 25
+      h = 6
       layers = find_layers(input.strip, w, h)
       # not 1364 (too low)
       layers.sort_by! { |v| v.count('0') }
       layers[0].count('1') * layers[0].count('2')
     end
 
-    def part2(input)
-      w,h = [25, 6]
-
-      layers = find_layers(input.strip, w, h)
-      # layers.map { |l| split_by(l, w)}
-      # w,h = [2, 2]
-      # layers = ["0222", "1122", "2212", "0000"]
-      remapped_layer = (0...(w*h)).map do |i|
+    def remap_layers(layers, w, h)
+      (0...(w * h)).map do |i|
         layers.map do |l|
           l[i]
         end.join
       end
+    end
+
+    def part2(input)
+      w = 25
+      h = 6
+
+      layers = find_layers(input.strip, w, h)
+      remapped_layer = remap_layers(layers, w, h)
 
       letters = remapped_layer.map do |s|
         s.split('').find { |v| v != '2' }
       end
       puts
-      screen = letters.map { |v| v == '1' ? '█' : ' '}
+      screen = letters.map { |v| v == '1' ? '█' : ' ' }
       screen.each_slice(w).map(&:join)
     end
   end

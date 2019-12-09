0ca5ade71c4ef067f76ff07b3d5b4232ee2c1686
clemkeirua <clement@keiruaprod.fr>
2019-12-09 22:59:33 +0100

    wip

--- src/year2019/int_code_program.rb
+++ src/year2019/int_code_program.rb
@@ -7,16 +7,36 @@ class IntCodeProgram
   def initialize(program = [])
     @ip = 0
     @program = program
+    @out = []
+  end
+
+  def decode_instruction(instruction)
+    instruction = instruction.to_s.rjust(5, '0')
+    mode_first = instruction[2].to_i
+    mode_second = instruction[1].to_i
+    mode_third = instruction[0].to_i
+    opcode = instruction[3..].to_i
+    [opcode, mode_first, mode_second, mode_third]
   end
 
   def step
-    a = @program[@program[@ip + 1]]
-    b = @program[@program[@ip + 2]]
-    case @program[@ip]
+    opcode, mode_first, mode_second, mode_third = decode_instruction(@program[@ip])
+
+    case opcode
     when 1
+      a = @program[@program[@ip + 1]]
+      b = @program[@program[@ip + 2]]
       @program[@program[@ip + 3]] = a + b
     when 2
+      a = @program[@program[@ip + 1]]
+      b = @program[@program[@ip + 2]]
       @program[@program[@ip + 3]] = a * b
+    when 3
+      a = @program[@program[@ip + 1]]
+      @program[a] = 1
+    when 4
+      a = @program[@program[@ip + 1]]
+      @out << @program[a]
     end
     @ip += 4
   end
